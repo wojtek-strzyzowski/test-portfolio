700eac1096ee7221b651344cd77b3c1f637de29f
[github-wojtek-strzyzowski] <[github-strzyzowski@gmx.de]>
2024-01-10 15:49:18 +0100

    front fertig

---
+++ script.js
@@ -0,0 +1,15 @@
+screen.addEventListener('unload', function(event) {
+    location.reload(6);
+});
+
+// document.getElementById('home-video').addEventListener('click', function() {
+//     if (this.requestFullscreen) {
+//         this.requestFullscreen();
+//     } else if (this.mozRequestFullScreen) {
+//         this.mozRequestFullScreen();
+//     } else if (this.webkitRequestFullscreen) {
+//         this.webkitRequestFullscreen();
+//     } else if (this.msRequestFullscreen) {
+//         this.msRequestFullscreen();
+//     }
+// });
